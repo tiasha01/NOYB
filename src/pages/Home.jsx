5e36aa261a5d8057a0a202ad466ba34df5797d6c
Vivek Dey <dey.vivek.official@gmail.com>
2024-12-01 19:53:28 +0530

    Updated the basic layout of the 'Home' page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,38 @@
+import {Box} from "@mui/material";
+
 function Home() {
     return (
-        <h1>Home page.</h1>
+        <>
+            <Box sx={{
+                display: 'flex',
+                width: '100vw',
+                height: '100vh',
+                backgroundColor: '#023626'
+            }}>
+                <Box sx={{ width: '30vw',
+                    height: '80vh',
+                    display: 'flex',
+                    margin: '5vh',
+                    marginTop: '15vh',
+                    marginRight: '2.5vh',
+                    borderRadius: '10px',
+                    justifyContent: 'flex-start',
+                    backgroundColor: '#00885F'
+                }}>
+                </Box>
+                <Box sx={{ width: '65vw',
+                    height: '80vh',
+                    display: 'flex',
+                    margin: '5vh',
+                    marginTop: '15vh',
+                    marginLeft: '2.5vh',
+                    borderRadius: '10px',
+                    justifyContent: 'flex-start',
+                    backgroundColor: '#00885F'
+                }}>
+                </Box>
+            </Box>
+        </>
     );
 }
 
